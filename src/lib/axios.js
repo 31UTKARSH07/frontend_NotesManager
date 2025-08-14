12286b8b731ba5cdeecccc93218344404379322c
Utkarsh <3107utkarshpathak@gmail.com>
2025-08-14 19:07:27 +0530

    changes in axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://your-backend-name.onrender.com/api";
-; // backend Render URL
+const BASE_URL = "https://backend-notesmanager-1.onrender.com/api"; // backend Render URL
 
 const api = axios.create({
   baseURL: BASE_URL,
