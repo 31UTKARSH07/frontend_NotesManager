c2e17deae605159705f14b841d1e3600e2c48cad
Utkarsh <3107utkarshpathak@gmail.com>
2025-08-14 13:10:44 +0530

    axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development"?"http://localhost:5001/api":"/api"
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api"
+  : "https://backend-notesmanager-1.onrender.com/api"; // backend Render URL
+
 const api = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 export default api;
