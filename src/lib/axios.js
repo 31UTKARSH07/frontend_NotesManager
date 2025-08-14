543f9c9e947a863370d04351aedc3e6b46d3fa78
Utkarsh <3107utkarshpathak@gmail.com>
2025-08-14 18:53:53 +0530

    Update API URL for Render deployment

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5001/api"
-  : "https://backend-notesmanager-1.onrender.com/api"; // backend Render URL
+const BASE_URL = "https://your-backend-name.onrender.com/api";
+; // backend Render URL
 
 const api = axios.create({
   baseURL: BASE_URL,
